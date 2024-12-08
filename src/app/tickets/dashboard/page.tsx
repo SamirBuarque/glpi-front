e87317556871c2f1db2036d2656a4da37d4fe4e0
Samir Buarque <samirbuarque@hotmail.com>
2024-12-08 13:29:07 -0300

    retornando os tickets do backend na rota tickets/dashboard

--- src/app/tickets/dashboard/page.tsx
+++ src/app/tickets/dashboard/page.tsx
@@ -1,15 +1,51 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import styles from "@/app/styles/layout/layout.module.css";
 import Header from "@/components/layout/header";
 import SideBar from "@/components/layout/sideBar";
 import Main from "@/components/layout/main";
 import Footer from "@/components/layout/footer";
+import axios from "axios";
 
-export default function DashboardPage() {
+const DashboardPage: React.FC = () => {
+  const [tickets, setTickets] = useState<any[]>([]);
+
+  const renderTickets = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5140/v1/tickets?pageNumber=1&pageSize=25", 
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("tentando exibir a response:")
+      console.log(response);
+      setTickets(response.data.data);
+    } catch (error) {
+      console.log("erro ao realizar o GET.", error);
+    }
+  };
+
+  useEffect(() => {
+    renderTickets();
+  }, []);
+
   return (
     <div>
-      <h1>main content</h1>
-      <p>aqui vai o conteudo principal da pagina</p>
+      <div>
+        {tickets.length > 0 ? (
+          tickets.map((ticket) => (
+            <div key={ticket.id}>
+              <h3>{ticket.title}</h3>
+              <p>{ticket.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>Nenhum ticket encontrado</p>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default DashboardPage;
